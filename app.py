3e9c59e51de5dde0e2f172237e09125887937f85
Terry Li <terryseanli@gmail.com>
2023-03-08 16:14:49 -0800

    works on silly and excited story

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
 from stories import silly_story
+from stories import excited_story
 
-STORY = silly_story
+STORY = excited_story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
