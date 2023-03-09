4631da6294de1198639e07e505436d0f52b6e601
Terry Li <terryseanli@gmail.com>
2023-03-08 16:09:47 -0800

    first draft of all app functions

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+
+from stories import silly_story
+
+STORY = silly_story
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+
+debug = DebugToolbarExtension(app)
+
+@app.get('/')
+def index():
+    """Return homepage"""
+
+    html = render_template(
+        'questions.html',
+        prompts=STORY.prompts)
+
+    return html
+
+@app.get('/results')
+def show_results():
+    """Return resulting STORY for user-provided answers"""
+
+    prompts = STORY.prompts
+    template = STORY.template
+
+    # create dictionary of {prompts: answers} from inputs
+    answers = {}
+    for prompt in prompts:
+        answers[prompt] = request.args[prompt]
+
+    result = STORY.generate(answers)
+
+    html = render_template(
+        'results.html',
+        result_story=result)
+
+    return html
